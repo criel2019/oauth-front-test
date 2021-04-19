be3a6b7d20a6373275bfa8fca25c48470c622430
criel2019 <pjhk579700@gmail.com>
2021-04-19 12:51:58 +0900

    Update NavBar.tsx

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -6,11 +6,19 @@ import { myContext } from '../../Context';
 import { IUser } from '../../types/maintypes';
 
 
+
 export default function NavBar() {
     const userObject = useContext(myContext) as IUser;
 
     const logout = () => {
         axios.get("https://criel-db-test.herokuapp.com/user/auth/logout", {
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "https://criel-db-test.herokuapp.com",
+                "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+                "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+                "Access-Control-Allow-Credentials": true,
+            },
             withCredentials: true
         }).then((res: AxiosResponse) => {
             if (res.data === "done") {
